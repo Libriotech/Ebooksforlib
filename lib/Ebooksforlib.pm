d260fd39e9173df154f3fc9162cf0157d5262632
Magnus Enger <magnus@enger.priv.no>
2013-01-16 10:30:24 +0100

    Add the skeleton for a dummy API for the reader-app #794

--- lib/Ebooksforlib.pm
+++ lib/Ebooksforlib.pm
@@ -15,4 +15,43 @@ get '/about' => sub {
     template 'about', { pagetitle => 'About' };
 };
 
+# Reader-app API
+# The server-side component of the reader-app will be talking to this API
+# TODO These are dummy functions with hardcoded responses, for now
+
+set serializer => 'JSON';
+
+get '/rest/login' => sub {
+
+    return { 
+        hash => '0c86b9ecb91ee1e16f96363956dff7e5'
+    };
+
+};
+
+get '/rest/logout' => sub {
+
+    return { 
+        logout => 1
+    };
+
+};
+
+get '/rest/listbooks' => sub {
+
+    return { 
+        1 => { title => 'a', author => 'b' },
+        2 => { title => 'c', author => 'd' },
+    };
+
+};
+
+get '/rest/getbook' => sub {
+
+    return { 
+        login => 'ok'
+    };
+
+};
+
 true;
