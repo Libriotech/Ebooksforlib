901f7840f883ed8ef02140d1acafa8e5d9083c99
Magnus Enger <magnus@enger.priv.no>
2013-01-21 15:59:31 +0100

    Start to add libraries to the mix

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 -- Database schema for Ebooksforlib
 
 DROP TABLE IF EXISTS user_roles;
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id       INTEGER     AUTO_INCREMENT PRIMARY KEY,
@@ -15,16 +16,24 @@ CREATE TABLE roles (
     role  VARCHAR(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS libraries;
+CREATE TABLE libraries (
+    id   INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE user_roles (
-    user_id  INTEGER NOT NULL,
-    role_id  INTEGER NOT NULL,
-    -- TODO library_id
+    user_id    INTEGER NOT NULL,
+    role_id    INTEGER NOT NULL,
+    library_id INTEGER,
     PRIMARY KEY user_role (user_id, role_id), 
     CONSTRAINT user_roles_fk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT user_roles_fk_2 FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+    CONSTRAINT user_roles_fk_2 FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    CONSTRAINT user_roles_fk_3 FOREIGN KEY (library_id) REFERENCES libraries (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Sample data
+-- TODO Split this out into a separate file
 
 -- Roles
 INSERT INTO roles SET id = 1, role = 'admin';
@@ -34,6 +43,10 @@ INSERT INTO roles SET id = 2, role = 'superadmin';
 INSERT INTO users SET id = 1, username = 'henrik', password = 'pass', name = 'Henrik Ibsen';
 INSERT INTO users SET id = 2, username = 'sigrid', password = 'pass', name = 'Sigrid Undset';
 
--- Assign roles to users
-INSERT INTO user_roles SET user_id = 1, role_id = 1; -- Henrik is admin
+-- Libraris
+INSERT INTO libraries SET id = 1, name = 'Storevik';
+INSERT INTO libraries SET id = 2, name = 'Lillevik';
+
+-- User roles
+INSERT INTO user_roles SET user_id = 1, role_id = 1, library_id = 1; -- Henrik is admin at Storevik
 INSERT INTO user_roles SET user_id = 2, role_id = 2; -- Sigrid is superadmin
---
+++ lib/Ebooksforlib/Schema/Result/Library.pm
@@ -0,0 +1,67 @@
+package Ebooksforlib::Schema::Result::Library;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+
+=head1 NAME
+
+Ebooksforlib::Schema::Result::Library
+
+=cut
+
+__PACKAGE__->table("libraries");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 name
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+);
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 user_roles
+
+Type: has_many
+
+Related object: L<Ebooksforlib::Schema::Result::UserRole>
+
+=cut
+
+__PACKAGE__->has_many(
+  "user_roles",
+  "Ebooksforlib::Schema::Result::UserRole",
+  { "foreign.library_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2013-01-21 15:58:34
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:iIzFrJMLnVILAeG+ilu6KA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
