8ee3d3cb0bb25b6decd12afbb10221956effac68
Magnus Enger <magnus@enger.priv.no>
2013-01-30 16:09:27 +0100

    Add vendor/ dir to path in bin/app.pl

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/env perl
 use Dancer;
 use Ebooksforlib;
+
+BEGIN {
+    use FindBin;
+
+    while ( my $libdir = glob("${FindBin::Bin}/../vendor/*/lib") ) {
+        unshift @INC, $libdir;
+    }
+}
+
 dance;
