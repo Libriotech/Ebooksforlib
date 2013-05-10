b2a9429d4d074bc1583b09f83604d30c5a97cd97
Magnus Enger <magnus@enger.priv.no>
2013-05-10 16:43:03 +0200

    Let users edit their own comments

--- lib/Ebooksforlib.pm
+++ lib/Ebooksforlib.pm
@@ -135,6 +135,54 @@ post '/comments/add' => require_login sub {
 
 };
 
+get '/comments/edit/:id' => sub {
+
+    my $comment_id  = param 'id';
+    my $user_id     = session( 'logged_in_user_id' );
+    
+    my $comment = rset('Comment')->find( $comment_id );
+    
+    # Check that the current user is the author of the comment
+    if ( $comment->user_id != $user_id ) {
+        flash error => "Are you trying to edit a comment you did not write?";
+        return redirect '/book/' . $comment->book->id;
+    }
+    
+    template 'comments_edit', { comment => $comment };
+
+};
+
+post '/comments/edit' => sub {
+
+    my $comment_id  = param 'comment_id';
+    my $comment_raw = param 'comment';
+    my $user_id     = session( 'logged_in_user_id' );
+    
+    # Make sure the user-submitted comment is safe
+    my $hs = HTML::Strip->new();
+    my $comment_safe = $hs->parse( $comment_raw );
+    
+    my $comment = rset('Comment')->find( $comment_id );
+    
+    # Check that the current user is the author of the comment
+    if ( $comment->user_id != $user_id ) {
+        flash error => "Are you trying to edit a comment you did not write?";
+    } else {
+    
+        try {
+            $comment->set_column( 'comment', $comment_safe );
+            $comment->update;
+            flash info => 'The comment was updated!';
+        } catch {
+            flash error => "Oops, we got an error:<br />$_"; # FIXME Don't show the raw error to end users
+            error "$_";
+        };
+        
+    }
+    redirect '/book/' . $comment->book->id;
+
+};
+
 get '/comments/new' => sub {
 
     my @comments = rset('Comment')->search( {}, {
