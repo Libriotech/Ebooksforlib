b486c33dc0c2ea67c8b68dd7cf7a6699cc4ccde6
Magnus Enger <magnus@enger.priv.no>
2014-09-24 14:11:30 +0200

    Finish work on deleting books

--- lib/Ebooksforlib/Route/Superadmin/DeleteBook.pm
+++ lib/Ebooksforlib/Route/Superadmin/DeleteBook.pm
@@ -13,11 +13,65 @@ use Dancer::Plugin::FlashMessage;
 use Dancer::Exception qw(:all);
 use Ebooksforlib::Util;
 
-get '/superadmin/delete/book/:id' => require_role superadmin => sub { 
+get '/superadmin/delete_book/:id' => require_role superadmin => sub { 
 
     my $book_id = param 'id';
     my $book = rset( 'Book' )->find( $book_id );
     template 'superadmin_delete_book', { book => $book };
+
+};
+
+get '/superadmin/delete_book_ok/:id' => require_role superadmin => sub { 
+
+    unless ( _check_csrftoken( param 'csrftoken' ) ) {
+        return redirect '/';
+    }
+
+    my $book_id = param 'id';
+    my $book = rset( 'Book' )->find( $book_id );
+    # Loop over the files
+    foreach my $file ( $book->files ) {
+        # Loop over the items
+        foreach my $item ( $file->items ) {
+            try {
+                # Check if there are any loans
+                if ( $item->loan ) {
+                    try {
+                        # Delete the loan first of all
+                        $item->loan->delete;
+                        info "Deleted loan with ID = " . $item->loan->id;
+                    } catch {
+                        flash error => "Oops, we got an error:<br />".errmsg($_);
+                        error "$_";
+                    };
+                }
+                # Now delete the item
+                $item->delete;
+                info "Deleted item with ID = " . $item->id;
+            } catch {
+                flash error => "Oops, we got an error:<br />".errmsg($_);
+                error "$_";
+            };
+        }
+        try {
+            # When all items connected to this file are deleted, we can delete the file
+            $file->delete;
+            info "Deleted file with ID = " . $file->id;
+        } catch {
+            flash error => "Oops, we got an error:<br />".errmsg($_);
+            error "$_";
+        };
+    }
+    try {
+        # All items and files should be gone by now, so let's delete the book itself
+        $book->delete;
+        flash info => 'A book was deleted!';
+        info "Deleted book with ID = $book_id";
+    } catch {
+        flash error => "Oops, we got an error:<br />".errmsg($_);
+        error "$_";
+    };
+    redirect '/superadmin';
 
 };
 
