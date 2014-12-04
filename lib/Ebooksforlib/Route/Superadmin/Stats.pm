10b12e353ac2b2daae6d8a9cc181c54d3a095cb3
Magnus Enger <magnus@enger.priv.no>
2014-12-04 21:04:34 +0100

    Add a list of most popular titles for superadmins
    This a basic fix, limited to 10 titles. Improvements to come.
    
    Fixes #22

--- lib/Ebooksforlib/Route/Superadmin/Stats.pm
+++ lib/Ebooksforlib/Route/Superadmin/Stats.pm
@@ -8,12 +8,54 @@ Routes for viewing stats.
 
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Database;
 use Dancer::Plugin::Auth::Extensible;
 use Dancer::Plugin::FlashMessage;
 use Dancer::Exception qw(:all);
 use Ebooksforlib::Util;
 
-### Lists
+get '/superadmin/stats/mostpop' => require_role superadmin => sub { 
+
+    # my @mostpop = resultset('Book')->search(
+    #     undef,
+    # {
+    #     join => { files => { items => 'old_loans' } },
+    #     select   => [ 'title', 'isbn', { count => 'old_loans.id' } ],
+    #     as       => [qw/ title isbn count /],
+    #     group_by => [qw/ isbn /],
+    #     order_by => { -desc => 'count' },
+    #     rows => 10,
+    # });
+    my $sth = database->prepare(
+'SELECT
+    b.id,
+    b.title,
+    b.isbn,
+    COUNT(*) as loans
+FROM
+    books AS b,
+    files AS f,
+    items as i,
+    old_loans as l
+WHERE
+    b.id = f.book_id AND
+    f.id = i.file_id AND
+    i.id = l.item_id
+GROUP BY
+    isbn
+ORDER BY
+    loans DESC
+LIMIT 10;'
+    );
+    $sth->execute();
+    my @mostpop;
+    while ( my $book = $sth->fetchrow_hashref ) {
+        push @mostpop, $book;
+    }
+    template 'superadmin_mostpop', { 
+        mostpop => \@mostpop,
+    };
+};
 
 get '/superadmin/stats/failed' => require_role superadmin => sub { 
 
